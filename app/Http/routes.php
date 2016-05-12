aca11748949c90296a5d3ba5dbd2e8d678fe3e5c
Bruno Neves <brunu.neves@gmail.com>
2016-05-12 09:35:23 -0300

    Create route tasks and migration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,6 +29,13 @@ Route::put('project/{id}/note/{noteId}', 'ProjectNoteController@update');
 Route::delete('project/{id}/note/{noteId}', 'ProjectNoteController@destroy');
 
 
+Route::get('project/{id}/task', 'ProjectTaskController@index');
+Route::post('project/{id}/task', 'ProjectTaskController@store');
+Route::get('project/{id}/task/{taskId}', 'ProjectTaskController@show');
+Route::delete('project/{id}/task/{taskId}', 'ProjectTaskController@destroy');
+Route::put('project/{id}/task/{taskId}', 'ProjectTaskController@update');
+
+
 Route::get('project', 'ProjectController@index');
 Route::post('project', 'ProjectController@store');
 Route::put('project/{id}', 'ProjectController@update');
