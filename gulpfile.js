a0260ab73d5ee18fd763259169a68918d8d2c14b
Bruno Neves <brunu.neves@gmail.com>
2016-06-03 15:56:57 -0300

    Autentication oauth2 angularjs and messages erros

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,10 @@ config.vendor_path_js = [
     config.bower_path + '/angular-animate/angular-animate.min.js',
     config.bower_path + '/angular-messages/angular-messages.min.js',
     config.bower_path + '/angular-bootstrap/ui-bootstrap.min.js',
-    config.bower_path + '/angular-strap/dist/modules/navbar.min.js'
+    config.bower_path + '/angular-strap/dist/modules/navbar.min.js',
+    config.bower_path + '/angular-cookies/angular-cookies.min.js',
+    config.bower_path + '/query-string/query-string.js',
+    config.bower_path + '/angular-oauth2/dist/angular-oauth2.min.js'
 ];
 
 config.build_path_css = config.build_path +'/css';
@@ -31,13 +34,23 @@ config.vendor_path_css = [
     config.bower_path + '/bootstrap/dist/css/bootstrap-theme.min.css'
 ];
 
+config.build_path_html = config.build_path +'/views';
+
+gulp.task('copy-html', function() {
+    gulp.src([
+            config.assets_path + '/js/views/**/*.html'
+        ])
+        .pipe(gulp.dest(config.build_path_html))
+        .pipe(liveReload());
+});
+
 /**
  * Tarefa para copiar styles CSS
  */
 gulp.task('copy-styles', function (){
     gulp.src([
-        config.assets_path + '/css/**/*.css'
-    ])
+            config.assets_path + '/css/**/*.css'
+        ])
         .pipe(gulp.dest(config.build_path_css))
         .pipe(liveReload());
 
@@ -74,13 +87,14 @@ gulp.task('clear-build-folder', function (){
  * Gulp default
  */
 gulp.task('default', ['clear-build-folder'], function(){
+    gulp.start('copy-html');
     elixir(function(mix){
 
         mix.styles(config.vendor_path_css.concat([config.assets_path + '/css/**/*.css']),
-        'public/css/all.css', config.assets_path);
+            'public/css/all.css', config.assets_path);
 
         mix.scripts(config.vendor_path_js.concat([config.assets_path + '/js/**/*.js']),
-        'public/js/all.js', config.assets_path);
+            'public/js/all.js', config.assets_path);
 
         mix.version(['js/all.js', 'css/all.css']);
     });
@@ -91,6 +105,8 @@ gulp.task('default', ['clear-build-folder'], function(){
  */
 gulp.task('watch-dev', ['clear-build-folder'], function(){
     liveReload.listen();
-    gulp.start('copy-styles','copy-scripts');
-    gulp.watch(config.assets_path + '/**', ['copy-styles', 'copy-scripts']);
+    gulp.start('copy-styles','copy-scripts','copy-html');
+    gulp.watch(config.assets_path + '/**', [
+        'copy-styles', 'copy-scripts', 'copy-html'
+    ]);
 });
